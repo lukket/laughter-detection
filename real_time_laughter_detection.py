52013cf275adc604b392e3af2ca541f01744adf3
Lukas Kettenbach <kettenbach@medialesson.de>
2023-05-23 12:26:51 +0200

    Optimize data processing
    - decouple recording and processing
    - use parameters from model
    - create visual output

--- real_time_laughter_detection.py
+++ real_time_laughter_detection.py
@@ -1,14 +1,14 @@
-# Example usage:
-# python segment_laughter.py --input_audio_file=tst_wave.wav --output_dir=./tst_wave --save_to_textgrid=False --save_to_audio_files=True --min_length=0.2 --threshold=0.5
-
 from os import path
 import torch
 import numpy as np
 import configs
+import laugh_segmenter
 from utils import audio_utils, data_loaders, torch_utils
 from functools import partial
 import pyaudio
 import datetime
+import time
+from collections import deque
 
 model_path = 'checkpoints/in_use/resnet_with_augmentation'
 config = configs.CONFIG_MAP['resnet_with_augmentation']
@@ -37,91 +37,115 @@ else:
 
 audio_system = pyaudio.PyAudio()  # start the PyAudio class
 
-MIN_CHUNK = 44*186  # number of data points to read at a time
+NUMBER_OF_FRAMES = 44
+HOP_LENGTH = 186
+CHUNK_SIZE = 1
+# number of data points to read at a time (number of frames * hop length?)
+CHUNK = CHUNK_SIZE*NUMBER_OF_FRAMES*HOP_LENGTH
+RATE = 8000  # time resolution of the recording device (Hz)
+# RATE = int(audio_system.get_device_info_by_index(0)['defaultSampleRate'])
 
-CHUNK = 1*MIN_CHUNK  # number of data points to read at a time
-# RATE = 8000  # time resolution of the recording device (Hz)
-RATE = int(audio_system.get_device_info_by_index(0)['defaultSampleRate'])
-print(f'Rate: {RATE}')
+max_number_of_audio_time_series = 4
+audio_time_series_buffer = deque([])
+
+print(f'Number of time series processed at a time: {max_number_of_audio_time_series}')
+
+def buffer_time_series(in_data, frame_count, time_info, status):
+    audio_time_series_buffer.append(np.frombuffer(in_data, dtype=np.float32))
+    return (in_data, status)
 
 
-def callback(in_data, frame_count=None, time_info=None, status=None):
-    data = np.frombuffer(in_data, dtype=np.float32)
+def process_time_series():
+    if (len(audio_time_series_buffer) > 2):
+        audio_time_series_buffer.clear()
+        print('Time series queue became too large (queue cleared). Adjust your parameters to avoid this issue.')
+        return
+
+    if (len(audio_time_series_buffer) < 1):
+        time.sleep(0.1)
+        return
+
+    audio_time_series = np.array(audio_time_series_buffer.popleft(), dtype=np.float32).flatten()
 
     # Load features
 
     inference_dataset = data_loaders.RealTimeInferenceDataset(
-        audio_time_series=data, feature_fn=feature_fn, sr=RATE)
+        audio_time_series=audio_time_series,
+        feature_fn=feature_fn,
+        sr=RATE,
+        n_frames=NUMBER_OF_FRAMES
+    )
 
-    collate_fn = partial(audio_utils.pad_sequences_with_labels,
-                         expand_channel_dim=config['expand_channel_dim'])
+    collate_fn = partial(
+        audio_utils.pad_sequences_with_labels,
+        expand_channel_dim=config['expand_channel_dim']
+    )
 
     inference_generator = torch.utils.data.DataLoader(
-        inference_dataset, num_workers=0, batch_size=8, shuffle=False, collate_fn=collate_fn)
+        inference_dataset,
+        num_workers=0,
+        batch_size=8,
+        shuffle=False,
+        collate_fn=collate_fn
+    )
 
     # Make Predictions
 
-    probs = []
+    probabilities = []
     for model_inputs, _ in inference_generator:
         x = torch.from_numpy(model_inputs).float().to(device)
-        preds = model(x).cpu().detach().numpy().squeeze()
-        if len(preds.shape) == 0:
-            preds = [float(preds)]
+        predictions = model(x).cpu().detach().numpy().squeeze()
+        if len(predictions.shape) == 0:
+            predictions = [float(predictions)]
         else:
-            preds = list(preds)
-        probs += preds
-    probs = np.array(probs)
+            predictions = list(predictions)
+        probabilities += predictions
+    probabilities = np.array(probabilities)
 
-    # probs = laugh_segmenter.lowpass(probs)
-    instances = get_laughter_instances(probs, threshold=threshold)
+    #probs = laugh_segmenter.lowpass(probs)
+    #instances = get_laughter_instances(probs, threshold=threshold)
 
-    return (in_data, status)
-
-
-def get_laughter_instances(probs, threshold=0.5, min_length=0.2, fps=100.) -> list:
-    instances = []
-    current_list = []
-    for i in range(len(probs)):
-        probability = np.min(probs[i:i+1])
-        percentage = int(probability * 100)
-        # print(f'Probability: {probability}')
-        timestamp = datetime.datetime.now()
+    laughers = []
+    for probability_index in range(len(probabilities)):
+        probability = np.min(probabilities[probability_index:probability_index+1])
         if probability > threshold:
-            print(f'{timestamp} {percentage}%: 🤣')
-            current_list.append(i)
-        else:
-            print(f'{timestamp} {percentage}%: .')
-            if len(current_list) > 0:
-                instances.append(current_list)
-                current_list = []
-    if len(current_list) > 0:
-        instances.append(current_list)
-    # instances = [frame_span_to_time_span(collapse_to_start_and_end_frame(i),fps=fps) for i in instances]
-    # instances = [inst for inst in instances if inst[1]-inst[0] > min_length]
-    return instances
+            laughers.append(probability)
 
+
+    number_of_probabilities = len(probabilities)
+    number_of_laughers = len(laughers)
+    if number_of_laughers > 0:
+        laugher_ratio = number_of_laughers/number_of_probabilities
+        laugher_ratio_string = '{:.2f}%'.format(laugher_ratio*100).rjust(7)
+
+        average_laugher_probability = np.average(laughers)
+        average_laugher_probability_string = '{:.2f}%'.format(average_laugher_probability*100).rjust(7)
+        average_laugher_probability_visual = ''
+        for _ in range(int(average_laugher_probability * 100)):
+            average_laugher_probability_visual += '|'
+        average_laugher_probability_visual = average_laugher_probability_visual.ljust(100, '.')
+
+        print(f'{datetime.datetime.now()} \t Ratio: {laugher_ratio_string} \t Average: {average_laugher_probability_string} {average_laugher_probability_visual}')
+    else:
+        print(f'{datetime.datetime.now()}')
 
 stream = audio_system.open(
     format=pyaudio.paFloat32,
     channels=1,
-    rate=RATE, input=True,
+    rate=RATE,
+    input=True,
     frames_per_buffer=CHUNK,
-    # stream_callback=callback
+    stream_callback=buffer_time_series
 )  # uses default input device
 
-# Wait for stream to finish (4)
-while stream.is_active():
-    try: 
-        in_data = stream.read(CHUNK)
-        callback(in_data=in_data)
+
+while True:
+    try:
+        process_time_series()
     # ctrl-c
     except KeyboardInterrupt:
-        exit()
-    # any different error
-    except RuntimeError as e:
-        print(e)
-
-# Close the stream (5)
-stream.close()
-# Release PortAudio system resources (6)
-audio_system.terminate()
+        stream.stop_stream()
+        stream.close()
+        # Release PortAudio system resources (6)
+        audio_system.terminate()
+        quit()
